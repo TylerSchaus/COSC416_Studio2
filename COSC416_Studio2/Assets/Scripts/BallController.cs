d8ede8e583fbcdb8cc26e5c890a8d7aaef859264
TylerSchaus <tylerdschaus@shaw.ca>
2025-02-01 16:41:21 -0800

    Launch indicator

--- COSC416_Studio2/Assets/Scripts/BallController.cs
+++ COSC416_Studio2/Assets/Scripts/BallController.cs
@@ -6,6 +6,7 @@ public class BallController : MonoBehaviour
     [SerializeField] private float force = 1f;
     [SerializeField] InputManager inputManager;
     [SerializeField] private Transform ballAnchor;
+    [SerializeField] private Transform launchIndicator;
 
     private bool isBallLaunched;
     private Rigidbody ballRB;
@@ -34,10 +35,12 @@ public class BallController : MonoBehaviour
         isBallLaunched = true;
 
         transform.parent = null;
-
+   
         ballRB.isKinematic = false;
 
-        ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
+        ballRB.AddForce(launchIndicator.forward * force, ForceMode.Impulse);
+
+        launchIndicator.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
